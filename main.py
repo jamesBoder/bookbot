7b3ee7c447ba3b02fc7ed44edbf8e1cb5b289c3c
jamesboder <james.macean08@gmail.com>
2025-02-17 16:26:12 -0500

    main.py update

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ def count_words():
         words = file_contents.split()
         word_count = len(words)
         print (word_count)
+        
+def string_number():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        case_lower = file_contents.lower()
+        char_count = {}
+        for i in case_lower:
+            if i in char_count:
+                char_count[i] += 1
+            else:
+                char_count[i] = 1
+    return char_count
+
+string_dict = string_number()
+            
+        
     
 if __name__ == "__main__":
-    main()
-    count_words()
+    
+    print(string_dict)
