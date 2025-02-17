f385f7299ba05c68e6d0c2daa72bb27ded13ca06
jamesboder <james.macean08@gmail.com>
2025-02-17 18:03:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = string_number()
+    print(chars_dict)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 def count_words():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         words = file_contents.split()
         word_count = len(words)
-        print (word_count)
+        return (word_count)
         
 def string_number():
     with open("books/frankenstein.txt") as f:
@@ -22,10 +28,35 @@ def string_number():
                 char_count[i] = 1
     return char_count
 
+total_words = count_words()
+
 string_dict = string_number()
-            
+
+def sort_on(dict):
+    return dict["num"]
+
+def dictionary (string_dict):
+    list_of_dicts = []
+    for char, count in string_dict.items():
+        if char.isalpha():
+            char_dict = {"char": char, "num": count} 
+            list_of_dicts.append(char_dict)
+    list_of_dicts.sort(key=sort_on, reverse=True)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words()} words found in the document")
+    for char_dict in list_of_dicts:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+    print ("--- End report ---")
         
+ 
+    
+    
+            
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
     
 if __name__ == "__main__":
     
-    print(string_dict)
+    
+	dictionary(string_dict)
